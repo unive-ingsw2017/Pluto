3980c142975609b51a1c567294669d6b25672ca2
Unknown <veneri.alberto@gmail.com>
2018-01-25 18:01:13 +0100

    Implementated class for Enti comparation

---
+++ app/src/main/java/mama/pluto/dataAbstraction/EntiComparator.java
@@ -0,0 +1,24 @@
+package mama.pluto.dataAbstraction;
+
+import java.util.List;
+import java.util.Set;
+
+public class EntiComparator {
+    private final EnteSummary ente1;
+    private final EnteSummary ente2;
+
+    public EntiComparator(EnteSummary ente1, EnteSummary ente2) {
+        this.ente1 = ente1;
+        this.ente2 = ente2;
+    }
+
+    private List<Category> getCategories(EnteSummary enteSummary){
+        return 
+    }
+
+    public List<Category> getSortedCategories{
+        for (: ente1.getEntrateMap()){
+
+        }
+    }
+}
--- sqlite-android/src/main/obj/local/armeabi-v7a/objs/sqlite3x/JNIString.o.d
+++
@@ -1,2 +0,0 @@
-C:/GIT/Pluto/sqlite-android/src/main/obj/local/armeabi-v7a/objs/sqlite3x/JNIString.o: \
-  C:/GIT/Pluto/sqlite-android/src/main/jni/sqlite/JNIString.cpp
