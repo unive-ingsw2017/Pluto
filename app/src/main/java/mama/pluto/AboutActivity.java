022ba3f8c4f4435687e8f315a9d05a922bd15408
Unknown <veneri.alberto@gmail.com>
2018-01-26 19:35:35 +0100

    about us update

--- app/src/main/java/mama/pluto/AboutActivity.java
+++ app/src/main/java/mama/pluto/AboutActivity.java
@@ -1,7 +1,9 @@
 package mama.pluto;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView version = findViewById(R.id.appVersionTV);
+        try {
+            version.setText(getPackageManager().getPackageInfo(getPackageName(), 0).versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
